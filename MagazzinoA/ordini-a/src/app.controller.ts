61ef55221f92720edd1bc0c2b79d31cb276eae07
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-03 16:37:22 +0200

    Update app.controller.ts

--- MagazzinoA/ordini-a/src/app.controller.ts
+++ MagazzinoA/ordini-a/src/app.controller.ts
@@ -5,8 +5,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async creaOrdine() {
-    return this.appService.creaOrdineConInventario();
+@Post('crea-ordine')
+  async creaOrdine(@Body() body: any) {
+    const ordine = {
+      id: Math.floor(Math.random() * 1000),
+      prodotto: body.prodotto,
+      quantita: body.quantita,
+    };
+
+    // 📢 Pubblica evento su NATS
+    this.client.emit('ordine_creato', ordine);
+    return { message: 'Ordine inviato', ordine };
+    console.log("Creato ordine di "+ quantita + " "+prodotto+ "(ID:"+id+")"); 
   }
 }
