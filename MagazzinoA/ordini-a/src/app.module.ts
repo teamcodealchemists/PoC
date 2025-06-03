9cb88a8b830f27946981a1554e910bdeadf0d19f
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-03 16:58:55 +0200

    Update app.module.ts

--- MagazzinoA/ordini-a/src/app.module.ts
+++ MagazzinoA/ordini-a/src/app.module.ts
@@ -1,19 +1,22 @@
+
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name: 'NATS_SERVICE',
+        name: 'ORDINE_SERVICE',           
         transport: Transport.NATS,
         options: {
-          servers: ['nats://nats:4222'],
-        },
+          servers: ['nats://nats:4222'],  
       },
     ]),
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
