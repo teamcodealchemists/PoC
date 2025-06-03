b4231bb420cbf630b775426ede1361253b8b9ef7
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-03 15:52:53 +0200

    Update app.service.ts

--- MagazzinoA/inventario-a/src/app.service.ts
+++ MagazzinoA/inventario-a/src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  verificaDisponibilita(prodottoId: number, quantita: number) {
+    // Logica fittizia
+    const disponibile = quantita <= 10;
+    return { disponibile };
   }
 }
