131e7b0184d5d742490b59f8946004e5f6c512a2
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-04 16:24:40 +0200

    Update main.ts

--- MagazzinoA/ordini-a/src/main.ts
+++ MagazzinoA/ordini-a/src/main.ts
@@ -16,5 +16,10 @@ async function bootstrap() {
   console.log('Microservizi ordini avviati');
   await app.listen(process.env.PORT ?? 3001); // Porta diversa da inventario
    console.log("AVVIATO CORRETTAMENTE MAIN ORDINI-A");
+
+  const appController = app.get(AppController);
+  setInterval(() => {
+    appController.inviaOrdineAutomatico();
+  }, 5000);
 }
 bootstrap();
