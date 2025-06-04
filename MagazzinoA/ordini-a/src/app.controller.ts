aa106d7d0e4a32abb0f83e6460da270893108e58
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-04 16:03:36 +0200

    Update app.controller.ts

--- MagazzinoA/ordini-a/src/app.controller.ts
+++ MagazzinoA/ordini-a/src/app.controller.ts
@@ -1,33 +1,33 @@
-import { Controller, Get, Body, Inject, Post  } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body, Inject, OnModuleInit } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
   constructor(
-  @Inject('ORDINE_SERVICE') private readonly client: ClientProxy,
-  private readonly appService: AppService
-) {}
+    @Inject('ORDINE_SERVICE') private readonly client: ClientProxy,
+    private readonly appService: AppService,
+  ) {}
 
-@Post('crea-ordine')
+  @Post('crea-ordine')
   async creaOrdine(@Body() body: any) {
     const ordine = {
       id: Math.floor(Math.random() * 1000),
       prodotto: body.prodotto,
       quantita: body.quantita,
     };
-}
-    async onModuleInit() {
-    setInterval(() => {
-      const ordine = { id: Date.now(), prodotto: 'banana', quantita: 1 };
-      this.client.emit('ordine_creato', ordine);
-      console.log('(GENERATO AUTOMATICAMENTE).. Evento ordine_creato inviato automaticamente', ordine);
-    }, 10000); // ogni 10 secondi
-  }
 
-    // 📢 Pubblica evento su NATS
+    // Pubblica evento su NATS
     this.client.emit('ordine_creato', ordine);
     console.log(`Creato ordine di ${ordine.quantita} ${ordine.prodotto} (ID: ${ordine.id})`);
     return { message: 'Ordine inviato', ordine };
-  
+  }
+
+  onModuleInit() {
+    setInterval(() => {
+      const ordine = { id: Date.now(), prodotto: 'banana', quantita: 1 };
+      this.client.emit('ordine_creato', ordine);
+      console.log('(GENERATO AUTOMATICAMENTE) Evento ordine_creato inviato automaticamente', ordine);
+    }, 10000); // ogni 10 secondi
+  }
 }
