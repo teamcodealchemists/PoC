83c10282f6cfad7d24d86209fe7a8397ab767ce1
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-06-09 15:15:44 +0200

    Test di inventario

--- MagazzinoA/inventario/src/app.service.ts
+++ MagazzinoA/inventario/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+//Risolvere il problema del linter che non trova il modulo ConfigService
+
+const ID_MAGAZZINO = new ConfigService().get('ID_MAGAZZINO'); // oppure usa DI
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'CIAO SONO IL MAGAZZINO ' + ID_MAGAZZINO;
   }
 }
