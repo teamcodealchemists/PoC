0e37613146afedf5ee6fa4d4b52bca84c8812a69
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-06-10 18:13:22 +0200

    Aggiunto log di test

--- microservizi/magazzino/inventario/src/app.service.ts
+++ microservizi/magazzino/inventario/src/app.service.ts
@@ -6,6 +6,7 @@ const ID_MAGAZZINO = new ConfigService().get('ID_MAGAZZINO');
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('Ciao sono un LOG di test');
     return 'Ciao Sono il microservizio Inventario '+ID_MAGAZZINO;
   }
 }
