99f3dd6a2da943f3399f29b688984e25ff28ac26
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-10 16:54:16 +0200

    Update app.module.ts

--- microservizi/magazzino/inventario/src/app.module.ts
+++ microservizi/magazzino/inventario/src/app.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://mongo:27017/inventario', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
+    MongooseModule.forFeature([
+      { name: 'categorie', schema: {} },
+      { name: 'inventario', schema: {} },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
