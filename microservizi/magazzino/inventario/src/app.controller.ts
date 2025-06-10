dd073de8e1331f665ed0a8a5615686a5de7c7c03
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-10 16:52:11 +0200

    Update app.controller.ts

--- microservizi/magazzino/inventario/src/app.controller.ts
+++ microservizi/magazzino/inventario/src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const quantitaAlimentari = await this.appService.getQuantitaProdottiAlimentari();
+    return `Ciao sono il magazzino ${this.appService.getIdMagazzino()} e ho ${quantitaAlimentari} prodotti di categoria Alimentari`;
   }
 }
