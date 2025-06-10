aa057a8cdd596863ef333d63d320ce8f01e9c834
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-10 18:25:48 +0200

    Update app.module.ts

--- microservizi/magazzino/inventario/src/app.module.ts
+++ microservizi/magazzino/inventario/src/app.module.ts
@@ -1,3 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { CategoriaSchema } from './schemas/categoria.schema';
+import { InventarioSchema } from './schemas/inventario.schema';
+
 import { Categoria, CategoriaSchema } from './schemas/categoria.schema';
 import { Inventario, InventarioSchema } from './schemas/inventario.schema';
 
