4f304140113f3a6922d5b6ec9da56ecc2b727a04
matteopretto <74592274+matteopretto@users.noreply.github.com>
2025-06-12 16:53:12 +0200

    Modifiche Database con rimozioni

--- microservizi/magazzino/mongo_db/init_db_inventario.js
+++ microservizi/magazzino/mongo_db/init_db_inventario.js
@@ -1,48 +1,37 @@
-
 db = db.getSiblingDB('inventario');
 
-db.categorie.insertMany([
-  { _id: 1, nome_categoria: "Elettronica" },
-  { _id: 2, nome_categoria: "Cancelleria" },
-  { _id: 3, nome_categoria: "Alimentari" },
-]);
-
-
-db.prodotti.insertMany([
-  { codice_barre: "10", nome_prodotto: "Mouse", prezzo_unitario: 15.99 },
-  { codice_barre: "20", nome_prodotto: "Penna", prezzo_unitario: 0.99 },
-  { codice_barre: "30", nome_prodotto: "Biscotti", prezzo_unitario: 2.50 },
-  { codice_barre: "31", nome_prodotto: "Caramelle", prezzo_unitario: 2.99 }
-]);
-
-
+// Inserimento diretto nella collezione `inventario`
 db.inventario.insertMany([
   {
     codice_barre: "10",
+    nome_prodotto: "Mouse",
+    prezzo_unitario: 15.99,
     quantita: 50,
     quantita_minima: 10,
-    quantita_massima: 100,
-    id_categoria: 1
+    quantita_massima: 100
   },
   {
     codice_barre: "20",
+    nome_prodotto: "Penna",
+    prezzo_unitario: 0.99,
     quantita: 200,
     quantita_minima: 50,
-    quantita_massima: 500,
-    id_categoria: 2
+    quantita_massima: 500
   },
   {
     codice_barre: "30",
+    nome_prodotto: "Biscotti",
+    prezzo_unitario: 2.50,
     quantita: 80,
     quantita_minima: 20,
-    quantita_massima: 200,
-    id_categoria: 3
+    quantita_massima: 200
   },
-    {
-    codice_barre: "31",
+  {
+    codice_barre: "40",
+    nome_prodotto: "Caramelle",
+    prezzo_unitario: 2.99,
     quantita: 80,
     quantita_minima: 20,
-    quantita_massima: 200,
-    id_categoria: 3
+    quantita_massima: 200
   }
 ]);
