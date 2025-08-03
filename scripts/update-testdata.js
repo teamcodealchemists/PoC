96125b0b3ec9c2c50399e0eca6c7964a9a6f6f9e
IwantBeRich <alex.shu@studenti.unipd.it>
2025-08-03 23:21:43 +0200

    tentativo di risolvere poc/poc t.5

--- scripts/update-testdata.js
+++ scripts/update-testdata.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function readCoverage(microservizio) {
-  const coveragePath = path.resolve(__dirname, `../../microservizi/magazzino/${microservizio}/coverage/coverage-summary.json`);
+  const coveragePath = path.resolve(__dirname, `../../microservizi/magazzino/${microservizio}/coverage/coverage-final.json`);
   return JSON.parse(fs.readFileSync(coveragePath, 'utf8')); 
 }
 
