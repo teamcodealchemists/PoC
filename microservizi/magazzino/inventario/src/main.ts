94b28acf0ba69671976cf195b04a757c24a3ade3
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-08-18 15:21:45 +0200

    Fix

--- microservizi/magazzino/inventario/src/main.ts
+++ microservizi/magazzino/inventario/src/main.ts
@@ -1,4 +1,3 @@
-import { response } from 'express';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
